52c30abf8c544dd110deb1ea7a5fa1e47d08ce5a
donblizy <nbliziotis@icloud.com>
2022-05-31 18:55:16 +0100

    component planning, home page progress

--- frontend_blizypin/src/client.js
+++ frontend_blizypin/src/client.js
@@ -7,6 +7,7 @@ export const client = sanityClient({
   apiVersion: "2022-05-31",
   useCdn: true,
   token: process.env.REACT_APP_SANITY_TOKEN,
+  ignoreBrowserTokenWarning: true,
 });
 
 const builder = imageUrlBuilder(client);
